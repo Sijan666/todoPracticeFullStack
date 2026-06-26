cc52198e3ab3bc6499831d341693d0043032eb76
Sijan666 <666majharulislam@gmail.com>
2026-06-27 02:32:12 +0600

    update design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,67 @@
+
 import { useState } from 'react'
-import './App.css'
 import axios from "axios"
+import './App.css'
+
 
 function App() {
 
-  let [task , setTask] = useState('')
-  let [priority , setPriority] = useState('')
-  let [status , setStatus] = useState('')
-  
-
+  let [task , setTask] = useState("")
+  let [priority , setPriority] = useState("")
+  let [status , setStatus] = useState("")
+ 
   const addTask = async () => {
     let data = await axios.post('http://localhost:5000/create/todo',{
       task : task,
       priority : priority,
       status : status
     })
-    console.log(data.data);
-    setTask("")
-    setPriority('')
-    setStatus('')
+    console.log(data);
+
+
+
   }
+
+  const handleTask = (e) => {
+    setTask(e.target.value)
+  }
+  const handlePriority = (e) => {
+    setPriority(e.target.value)
+  }
+  const handleStatus = (e) => {
+    setStatus(e.target.value)
+  }
+
+
 
   return (
     <>
-      <div className="max-w-200 mx-auto text-center">
-        <div className="text-center mt-10">
-          <h1 className='text-[48px] font-bold'>Todo</h1>
-        </div>
-        <div className="grid grid-cols-4 gap-20 mt-10 w-150 mx-auto justify-center">
-          <div className="">
-            <input type="text" className='px-4 py-1 border border-gray-300 outline-none' placeholder='Add task here' value={task}/>
-          </div>
-          <div className="flex gap-2">
-            <div className="">
-              <select className='border border-gray-400' value={status}>
-                <option value="active">Active</option>
-                <option value="pending">Pending</option>
-                <option value="block">Block</option>
-              </select>
-            </div>
-            <div className="">
-              <select className='border border-gray-400' value={priority}>
-                <option value="Low">Low</option>
-                <option value="Medium">Medium</option>
-                <option value="High">High</option>
-              </select>
-            </div>
-          </div>
-          <div className="btn">
-            <button onClick={addTask} className='py-1 px-3 bg-gray-500 text-white w-25 cursor-pointer'>Add Task</button>
-          </div>
-        </div>
-      </div>
+
+
+    <div className="max-w-200 mx-auto">
+
+      <h1>Todo</h1>
+
+      <input type="text" onChange={handleTask} value={task}/>
+
+      <select onChange={handlePriority} value={priority}>
+        <option value="low">low</option>
+        <option value="high">high</option>
+        <option value="medium">medium</option>
+      </select>
+
+
+      <select onChange={handleStatus} value={status}>
+        <option value="block">block</option>
+        <option value="pending">pending</option>
+        <option value="active">active</option>
+      </select>
+
+
+      <button onClick={addTask}>Add</button>
+    </div>
+
+
     </>
   )
 }
