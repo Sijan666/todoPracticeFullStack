c5ed266754c64a9020bac04d982f8c460a9b2c80
Sijan666 <666majharulislam@gmail.com>
2026-06-26 23:55:42 +0600

    add all required

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react'
 import './App.css'
+import axios from "axios"
 
 function App() {
 
+  let [task , setTask] = useState('')
+  let [priority , setPriority] = useState('')
+  let [status , setStatus] = useState('')
+  
+
+  const addTask = async () => {
+
+    let data = await axios.post('http://localhost:5000/create/todo',{
+      task : task,
+      priority : priority,
+      status : status
+    })
+    console.log(data.data);
+    setTask(data.data)
+    
+  }
 
   return (
     <>
@@ -9,19 +27,28 @@ function App() {
         <div className="text-center mt-10">
           <h1 className='text-[48px] font-bold'>Todo</h1>
         </div>
-        <div className="grid grid-cols-3 gap-5 mt-10 w-150 mx-auto justify-center">
+        <div className="grid grid-cols-4 gap-20 mt-10 w-150 mx-auto justify-center">
           <div className="">
             <input type="text" className='px-4 py-1 border border-gray-300 outline-none' placeholder='Add task here'/>
           </div>
-          <div className="">
-            <select className='border border-gray-400'>
-              <option value="Low">Low</option>
-              <option value="Medium">Medium</option>
-              <option value="High">High</option>
-            </select>
+          <div className="flex gap-2">
+            <div className="">
+              <select className='border border-gray-400'>
+                <option value="active">Active</option>
+                <option value="pending">Pending</option>
+                <option value="block">Block</option>
+              </select>
+            </div>
+            <div className="">
+              <select className='border border-gray-400'>
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>
+              </select>
+            </div>
           </div>
           <div className="btn">
-          <button className='py-1 px-3 bg-gray-500 text-white w-25 cursor-pointer'>Add Task</button>
+            <button onClick={addTask} className='py-1 px-3 bg-gray-500 text-white w-25 cursor-pointer'>Add Task</button>
           </div>
         </div>
       </div>
