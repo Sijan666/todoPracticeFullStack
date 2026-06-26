e5d679b4564b8f9ba9a396e8100bba93d060fe51
Sijan666 <666majharulislam@gmail.com>
2026-06-26 22:34:57 +0600

    add structure

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const app = express()
 
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0').then(()=>{
+    console.log('Database Connected');
+})
+
+
+
+
+
+
+app.listen(5000,()=>{
+    console.log('server is running');
+})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,25 @@ function App() {
 
   return (
     <>
-      <div className="">
-        <h1>Todo</h1>
+      <div className="max-w-200 mx-auto text-center">
+        <div className="text-center mt-10">
+          <h1 className='text-[48px] font-bold'>Todo</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-5 mt-10 w-150 mx-auto justify-center">
+          <div className="">
+            <input type="text" className='px-4 py-1 border border-gray-300 outline-none' placeholder='Add task here'/>
+          </div>
+          <div className="">
+            <select className='border border-gray-400'>
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+              <option value="High">High</option>
+            </select>
+          </div>
+          <div className="btn">
+          <button className='py-1 px-3 bg-gray-500 text-white w-25 cursor-pointer'>Add Task</button>
+          </div>
+        </div>
       </div>
     </>
   )
