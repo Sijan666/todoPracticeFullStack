86ea146ecfc27bb0914aee7730ac15f02ea2840e
Sijan666 <666majharulislam@gmail.com>
2026-06-28 01:04:39 +0600

    update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,12 @@
-import { useState } from 'react'
 import './App.css'
-
-import axios from 'axios'
 
 
 function App() {
 
-  let [task,setTask] = useState('')
-  let [priority,setPriority] = useState('')
-
-  const addTask = async () => {
-    let data  =  await axios.post('http://localhost:5000/create/todo' , {
-      task : task,
-      priority : priority
-    })
-    console.log(data);
-  }
-
-  const handleTask = (e)=>{
-    setTask(e.target.value)
-  }
-  const handlePriority = (e)=>{
-    setPriority(e.target.value)
-  }
-
 
   return (
     <>
-    <div className="max-w-300 mx-auto">
-      <div className="mt-15 flex justify-center gap-5">
-        <input onChange={handleTask} type="text" className='border-gray-400 border outline-none'/>
-        <select onChange={handlePriority}>
-          <option value="low">low</option>
-          <option value="medium">medium</option>
-          <option value="high">high</option>
-        </select>
-        <button onClick={addTask} className='border border-gray-400 bg-gray-500'>Add task</button>
-      </div>
-    </div>
+    <h1>Hello Frontend</h1>
     </>
   )
 }
