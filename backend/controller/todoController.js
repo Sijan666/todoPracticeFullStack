24369cae8eb975d6a63897eceb853c39fba4701e
Sijan666 <666majharulislam@gmail.com>
2026-06-28 23:30:11 +0600

    create controller

---
+++ backend/controller/todoController.js
@@ -0,0 +1,25 @@
+const Todo = require('../model/todoModel')
+
+const createTodo = async (req,res) => {
+    const {task,priority} = req.body
+
+    if (!task || !priority) {
+        return res.send({
+            success : true,
+            message : "please fill all the fields"
+        })
+    }
+
+    const todo = new Todo({
+        task : task,
+        priority : priority
+    })
+
+    await todo.save()
+    res.send({
+        success : true,
+        message : "todo created"
+    })
+}
+
+module.exports = {createTodo}
