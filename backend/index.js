dbfa3f94b5956ebac43ab5ccdad60454067ad232
Sijan666 <666majharulislam@gmail.com>
2026-07-05 04:24:57 +0600

    add image middleware

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const { createTodo, allTodos, deleteData, updateData } = require('./controller/todoController')
+const { createTodo } = require('./controller/todoController')
 const app = express()
+const multer = require('multer')
+
 
 app.use(express.json())
 app.use(cors())
+app.use('/uploads', express.static('uploads'));
+
 
 mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0').then(()=>{
     console.log('Database Connected');
 })
+
+
+const storage = multer.diskStorage({
+    destination:function (req,file,cb) {
+        cb(null,'./uploads')
+    },
+    filename:function (req,file,cb) {
+        let uniqueName = 'img'+"-"+Date.now() 
+        cb(null,uniqueName + "-" + file.originalname)
+    }
+})
+const upload = multer({ storage: storage });
+
+app.post('/createTodo' , upload.single('image') , createTodo)
 
 
 app.listen(5000,()=>{
