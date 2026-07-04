1da239e756b0e1b364c6cd486678aaba72044b9e
Sijan666 <666majharulislam@gmail.com>
2026-07-04 23:35:12 +0600

    remove all route

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,6 @@ mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzeks
     console.log('Database Connected');
 })
 
-app.post('/createTodo' , createTodo)
-app.get('/allTodos' , allTodos)
-app.delete('/deleteData/:id' , deleteData)
-app.post('/updateData/:id' , updateData)
 
 app.listen(5000,()=>{
     console.log('server is running');
