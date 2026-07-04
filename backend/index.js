d667da6d619e9a8ac9100da230ddc66ee437a1ea
Sijan666 <666majharulislam@gmail.com>
2026-07-04 23:24:56 +0600

    add delete controller in index

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const { createTodo, allTodos } = require('./controller/todoController')
+const { createTodo, allTodos, deleteData } = require('./controller/todoController')
 const app = express()
 
 app.use(express.json())
@@ -15,6 +15,7 @@ mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzeks
 
 app.post('/createTodo' , createTodo)
 app.get('/allTodos' , allTodos)
+app.delete('/deleteData' , deleteData)
 
 app.listen(5000,()=>{
     console.log('server is running');
