55fc0a573f9188c8c041729ab77861729ec53119
Sijan666 <666majharulislam@gmail.com>
2026-07-05 23:33:39 +0600

    update index

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 // require('node:dns').setServers(['1.1.1.1'] , ['8.8.8.8'])
-
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -7,16 +6,13 @@ const { createTodo } = require('./controller/todoController')
 const app = express()
 const multer = require('multer')
 
-
 app.use(express.json())
 app.use(cors())
 app.use('/uploads', express.static('uploads'));
 
-
 mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0').then(()=>{
     console.log('Database Connected');
 })
-
 
 const storage = multer.diskStorage({
     destination:function (req,file,cb) {
@@ -27,6 +23,7 @@ const storage = multer.diskStorage({
         cb(null,uniqueName + "-" + file.originalname)
     }
 })
+
 const upload = multer({ storage: storage });
 
 app.post('/createTodo' , upload.single('image') , createTodo)
