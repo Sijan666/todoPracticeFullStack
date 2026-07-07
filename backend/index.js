265aea804b0bd2e6661ada4abdeabc5612870046
Sijan666 <666majharulislam@gmail.com>
2026-07-07 22:10:36 +0600

    update index

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,36 @@
-// require('node:dns').setServers(['1.1.1.1'] , ['8.8.8.8'])
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const { createTodo } = require('./controller/todoController')
-const app = express()
-const multer = require('multer')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const multer = require('multer');
+const { createTodo, getTodos } = require('./controller/todoController');
 
-app.use(express.json())
-app.use(cors())
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
-mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0').then(()=>{
-    console.log('Database Connected');
-})
+
+mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0')
+    .then(() => console.log('Database Connected'))
+    .catch((err) => console.log('Database connection error:', err));
 
 const storage = multer.diskStorage({
-    destination : function (req,file,cb) {
-        cb(null,'./uploads')
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
     },
-    filename : function (req,file,cb) {
-        let uniqueName = 'img' + '-' + Date.now()
-        cb(null,uniqueName + '-' + file.originalname)
+    filename: function (req, file, cb) {
+        let uniqueName = 'img-' + Date.now();
+        cb(null, uniqueName + '-' + file.originalname);
     }
-})
+});
 
 const upload = multer({ storage: storage });
 
-app.post('/createTodo' , upload.single('image') , createTodo)
+// API Routes
+app.post('/createTodo', upload.single('image'), createTodo);
+app.get('/getTodos', getTodos);
 
-app.listen(5000,()=>{
-    console.log('server is running');
-})
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
