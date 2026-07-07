c492976bb167f24653b83f612902b9f2dbd4c9c2
Sijan666 <666majharulislam@gmail.com>
2026-07-07 22:09:52 +0600

    change controller

--- backend/controller/todoController.js
+++ backend/controller/todoController.js
@@ -1,34 +1,37 @@
-const Todo = require('../model/todoModel')
-
+const Todo = require('../model/todoModel');
 
 const createTodo = async (req, res) => {
     const { task, priority } = req.body;
 
     if (!task || !priority) {
-        return res.send({
-            success: false,
-            message: "Please fill all the fields"
-        });
+        return res.send({ success: false, message: "Please fill all the fields" });
     }
 
     if (!req.file) {
-        return res.send({
-            success: false,
-            message: "Please upload an image"
-        });
+        return res.send({ success: false, message: "Please upload an image" });
     }
 
-    const todo = new Todo({
-        task: task,
-        priority: priority,
-        path: req.file.path
-    });
+    try {
+        const todo = new Todo({
+            task: task,
+            priority: priority,
+            path: req.file.path
+        });
 
-    await todo.save();
-    res.send({
-        success: true,
-        message: 'Todo created successfully'
-    });
+        await todo.save();
+        res.send({ success: true, message: 'Todo created successfully' });
+    } catch (error) {
+        res.send({ success: false, message: 'Error creating todo' });
+    }
 };
 
-module.exports = { createTodo };
+const getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find().sort({ _id: -1 }); 
+        res.send({ success: true, todos: todos });
+    } catch (error) {
+        res.send({ success: false, message: "Error fetching tasks" });
+    }
+};
+
+module.exports = { createTodo, getTodos };
