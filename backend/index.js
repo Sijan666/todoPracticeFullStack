4f6be535d93ea0e4deadaff0676bd25a5d9cd2e7
Sijan666 <666majharulislam@gmail.com>
2026-07-10 23:03:09 +0600

    add update controller

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
-const { createTodo, getTodos, deleteTodos } = require('./controller/todoController');
+const { createTodo, getTodos, deleteTodos, updateData } = require('./controller/todoController');
 
 const app = express();
 
@@ -30,7 +30,8 @@ const upload = multer({ storage: storage });
 // API Routes
 app.post('/createTodo', upload.single('image'), createTodo);
 app.get('/getTodos', getTodos);
-app.delete('/deleteTask/:id', deleteTodos); 
+app.delete('/deleteTask/:id', deleteTodos);
+app.post('/updateData/:id', updateData)
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
