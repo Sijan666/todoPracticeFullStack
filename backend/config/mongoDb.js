aa29fd9073f9d6a9f3ab8705a0599f64fde938de
Sijan666 <666majharulislam@gmail.com>
2026-07-14 00:11:09 +0600

    update mongo

--- backend/config/mongoDb.js
+++ backend/config/mongoDb.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 // mongo atlas server
-mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0')
+const mongoDb = ()=>{
+    return mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0')
     .then(() => console.log('Database Connected successfully'))
     .catch((error) => console.log('Database connection failed:', error));
+}
+
+module.exports = mongoDb
