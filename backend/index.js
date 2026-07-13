252d735c27609a21379edb6b9ca343786b7232cd
Sijan666 <666majharulislam@gmail.com>
2026-07-14 00:08:00 +0600

    update index

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,6 @@
 // require("node:dns").setServers(['1.1.1.1'] , ['8.8.8.8'])
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const multer = require('multer');
 const { createTodo, deleteTodos, updateData, allTodo } = require('./controller/todoController');
 const storage = require('./utlis/storage')
 
@@ -12,10 +10,6 @@ app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
-// mongo atlas server
-mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0')
-    .then(() => console.log('Database Connected successfully'))
-    .catch((error) => console.log('Database connection failed:', error));
 
 // api routes
 app.post('/createTodo', upload.single('image'), createTodo);
