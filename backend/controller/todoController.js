861335e1855a8a82228426cd549d5e8f2c6a5fc9
Sijan666 <666majharulislam@gmail.com>
2026-07-13 22:41:13 +0600

    update : add status and use try catch in all controller

--- backend/controller/todoController.js
+++ backend/controller/todoController.js
@@ -1,66 +1,98 @@
 const Todo = require('../model/todoModel');
 
 // create task
-const createTodo = async (req,res) => {
-    const {task , priority} = req.body
-    // const fileType = req.file.mimetype
-    
-    if (!task || !priority) {
-        return res.send({
-            success : false,
-            message : "task and priority fields are required"
-        })
-    }
+const createTodo = async (req, res) => {
+    try {
+        const { task, priority } = req.body;
+        // const fileType = req.file.mimetype;
+        
+        if (!task || !priority) {
+            return res.send({
+                success: false,
+                message: "task and priority fields are required"
+            });
+        }
 
-    if (!req.file) {
+        if (!req.file) {
+            return res.send({
+                success: false,
+                message: "please upload a media file"
+            });
+        }
+
+        const todos = new Todo({
+            task: task,
+            priority: priority,
+            path: req.file.path,
+            // mediaType: fileType
+        });
+
+        await todos.save();
         res.send({
-            success : false,
-            message : "please upload a media file"
-        })
+            success: true,
+            message: "Task created successfully"
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Error occurred while creating task",
+            error: error.message
+        });
     }
-
-const todos = new Todo({
-    task: task,
-    priority: priority,
-    path: req.file.path,
-    // mediaType: fileType
-});
-
-    await todos.save()
-    res.send({
-        success : true,
-        message : "Task created successfully"
-    })
 }
 
 // alltasks
-const allTodo = async (req,res) => {
-    const data = await Todo.find({})
-    res.send({
-        success : true,
-        message : "All tasks fetched successfully",
-        data : data
-    })
+const allTodo = async (req, res) => {
+    try {
+        const data = await Todo.find({});
+        res.send({
+            success: true,
+            message: "All tasks fetched successfully",
+            data: data
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Error occurred while fetching tasks",
+            error: error.message
+        });
+    }
 }
 
 // deletetasks
 const deleteTodos = async (req, res) => {
-    let { id } = req.params;
-    await Todo.findByIdAndDelete(id); 
-    res.send({
-        success: true,
-        message: 'Task deleted successfully'
-    });
+    try {
+        let { id } = req.params;
+        await Todo.findByIdAndDelete(id); 
+        res.send({
+            success: true,
+            message: 'Task deleted successfully'
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Error occurred while deleting task",
+            error: error.message
+        });
+    }
 }
 
 // updatetasks
 const updateData = async (req, res) => {
-    const { id } = req.params;
-    const updateTask = await Todo.findByIdAndUpdate({_id:id},req.body)
-    res.send({
-        success: true,
-        message: "Task updated successfully"
-    });
+    try {
+        const { id } = req.params;
+        const updateTask = await Todo.findByIdAndUpdate({ _id: id }, req.body);
+        res.send({
+            success: true,
+            message: "Task updated successfully"
+        });
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Error occurred while updating task",
+            error: error.message
+        });
+    }
 }
 
-module.exports = { createTodo, allTodo , deleteTodos , updateData };
+module.exports = { createTodo, allTodo, deleteTodos, updateData };
