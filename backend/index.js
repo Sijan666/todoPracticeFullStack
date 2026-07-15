e9159b1e94a4aef884c309334bd099b23ddbd288
Sijan666 <666majharulislam@gmail.com>
2026-07-15 23:25:23 +0600

    import all api route in index

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 // require("node:dns").setServers(['1.1.1.1'] , ['8.8.8.8'])
 const express = require('express');
 const cors = require('cors');
-const { createTodo, deleteTodos, updateData, allTodo } = require('./controller/todoController');
-const upload = require('./utlis/storage')
-const mongoDb = require('./config/mongoDb')
+const mongoDb = require('./config/mongoDb');
+const todoRoutes = require('./routes/todoRoute');
 
 const app = express();
 
@@ -11,13 +10,10 @@ app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
-mongoDb()
+mongoDb();
 
-// api routes
-app.post('/createTodo', upload.single('image'), createTodo);
-app.get('/allTodo', allTodo);
-app.delete('/deleteTask/:id', deleteTodos);
-app.post('/updateData/:id', upload.single('image'), updateData);
+// api route
+app.use('/', todoRoutes); 
 
 // server
 app.listen(5000, () => {
