84f549724dff949e70e7a3808a3955ab299f39b1
Sijan666 <666majharulislam@gmail.com>
2026-07-17 20:32:21 +0600

    update db

--- backend/config/mongoDb.js
+++ backend/config/mongoDb.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const mongoDb = ()=>{
     return mongoose.connect('mongodb+srv://666majharulislam_db_user:25250180@cluster0.nzekssh.mongodb.net/todo?appName=Cluster0')
     .then(() => console.log('Database Connected successfully'))
-    .catch((error) => console.log('Database connection failed:', error));
+    .catch(err =>{
+        console.log(err);
+    })
 }
 
 module.exports = mongoDb
