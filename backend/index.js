a0417d4fc5e33d7b98c75ee440ca73087fd00857
Sijan666 <666majharulislam@gmail.com>
2026-07-19 23:31:25 +0600

    update index

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const mongoDb = require('./config/mongoDb');
 const todoRoutes = require('./routes/todoRoute');
-
 const app = express();
 
 // middlewares
